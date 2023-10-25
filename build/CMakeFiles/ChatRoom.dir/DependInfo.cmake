0ee01120b68608f9ca124558e03503a072d1df32
Danil Mikhaylov <damikh13@gmail.com>
2023-10-25 18:33:43 +0300

    add build configuration

---
+++ build/CMakeFiles/ChatRoom.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/ChatRoom.cpp" "CMakeFiles/ChatRoom.dir/src/ChatRoom.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/ChatRoom.cpp.o.d"
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/ConfigParser.cpp" "CMakeFiles/ChatRoom.dir/src/ConfigParser.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/ConfigParser.cpp.o.d"
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/Logger.cpp" "CMakeFiles/ChatRoom.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/Logger.cpp.o.d"
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/Message.cpp" "CMakeFiles/ChatRoom.dir/src/Message.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/Message.cpp.o.d"
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/User.cpp" "CMakeFiles/ChatRoom.dir/src/User.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/User.cpp.o.d"
+  "/Users/damikh13/Documents/University/year 2/sem1/Computer Science/chat_room_project/src/main.cpp" "CMakeFiles/ChatRoom.dir/src/main.cpp.o" "gcc" "CMakeFiles/ChatRoom.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ChatRoom.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ChatRoom.
+# This may be replaced when dependencies are built.
