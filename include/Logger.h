bbacd76e545cb511d8e4ab9db5c03507c33544c2
Danil Mikhaylov <damikh13@gmail.com>
2023-11-01 22:13:39 +0300

    get_current_time is now public

--- include/Logger.h
+++ include/Logger.h
@@ -18,18 +18,17 @@ public:
         ERROR
     };
 
-    Logger(log_level logLevel = log_level::INFO, const std::string& who_is_logging = "unknown");
+    Logger(const std::string& file_path, log_level logLevel = log_level::INFO);
     ~Logger();
 
     static void set_log_level(log_level level);
     void log(log_level level, const std::string& message);
 
+    static std::string get_current_time();
 private:
     static log_level current_log_level;
     std::ofstream log_file;
-    std::string who_is_logging;
 
-    static std::string get_current_time();
     std::map<log_level, std::string> log_level_to_string = {
         {log_level::DEBUG, "DEBUG"},
         {log_level::INFO, "INFO"},
