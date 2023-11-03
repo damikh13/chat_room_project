5a457821ec97ddecd52b2270e03d972fbf006861
Danil Mikhaylov <damikh13@gmail.com>
2023-11-03 23:24:58 +0300

    + config parsering

--- include/ConfigParser.h
+++ include/ConfigParser.h
@@ -1,13 +1,28 @@
 #ifndef CONFIGPARSER_H
 #define CONFIGPARSER_H
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "Logger.h"
+
 class ConfigParser
 {
-private:
-    /* data */
 public:
-    ConfigParser(/* args */);
-    ~ConfigParser();
+    // CONSTRUCTOR AND DESTRUCTOR
+    ConfigParser(const std::string& config_file_path, Logger& logger);  // Default constructor
+    ~ConfigParser();                                    // Default destructor
+
+    // HELPER FUNCTIONS
+    int get_int(const std::string& key);            // Get integer value from config file
+    std::string get_string(const std::string& key); // Get string value from config file
+
+private:
+    std::string filename_;                      // Path to config file
+    std::map<std::string, std::string> config_; // Map to store key-value pairs
+
+    void read_config_file(Logger& logger); // Read config file and store key-value pairs in map
 };
 
 #endif // !CONFIGPARSER_H
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,14 +1,54 @@
 #include "ConfigParser.h"
-#include <iostream>
 
-// Default constructor
-ConfigParser::ConfigParser(/* args */)
+// CONSTRUCTOR AND DESTRUCTOR
+ConfigParser::ConfigParser(const std::string& config_file_path, Logger& logger) // Default constructor
+    : filename_(config_file_path)
 {
-    std::cout << "ConfigParser constructor()" << std::endl;
+    read_config_file(logger);
+}
+ConfigParser::~ConfigParser()                                   // Default destructor
+{
 }
 
-// Default destructor
-ConfigParser::~ConfigParser()
+// HELPER FUNCTIONS
+int ConfigParser::get_int(const std::string& key)               // Get integer value from config file
 {
-    std::cout << "ConfigParser destructor()" << std::endl;
+    return std::stoi(config_[key]);
+}
+std::string ConfigParser::get_string(const std::string& key)    // Get string value from config file
+{
+    return config_[key];
+}
+
+void ConfigParser::read_config_file(Logger& logger)
+{
+    logger.log(Logger::log_level::DEBUG, "Reading config file...");
+    std::ifstream config_file(filename_);
+    if (!config_file.is_open())
+    {
+        logger.log(Logger::log_level::ERROR, "Could not open config file: " + filename_);
+        return;
+    }
+
+    std::string line;
+    while (std::getline(config_file, line))
+    {
+        bool needs_to_be_skipped = (line.empty() || line[0] == '#');
+        if (needs_to_be_skipped)
+        {
+            continue;
+        }
+
+        size_t delimiter_pos = line.find('=');
+        bool has_delimiter = (delimiter_pos != std::string::npos);
+        if (has_delimiter)
+        {
+            std::string key = line.substr(0, delimiter_pos - 1);
+            std::string value = line.substr(delimiter_pos + 2);
+            config_[key] = value;
+            logger.log(Logger::log_level::DEBUG, "Read key-value pair: [" + key + "][" + value + "]");
+        }
+    }
+
+    logger.log(Logger::log_level::DEBUG, "...Finished reading config file");
 }
