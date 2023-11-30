56f373e2e38ca1a2a55c1eb88e1541dc0135a423
Danil Mikhaylov <damikh13@gmail.com>
2023-11-30 23:07:08 +0300

    gpt modified .cfg handler

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -33,18 +33,36 @@ void ConfigParser::read_config_file(Logger& logger)
     std::string line;
     while (std::getline(config_file, line))
     {
-        bool needs_to_be_skipped = (line.empty() || line[0] == '#');
+        if (!line.empty() && line.back() == '\r')
+        {
+            line.pop_back();
+        }
+
+        line.erase(0, line.find_first_not_of(" \t"));
+        line.erase(line.find_last_not_of(" \t") + 1);
+
+        // Skip empty lines and comments
+        bool needs_to_be_skipped = line.empty() || line[0] == '#';
         if (needs_to_be_skipped)
         {
             continue;
         }
 
+        // Find the delimiter position
         size_t delimiter_pos = line.find('=');
-        bool has_delimiter = (delimiter_pos != std::string::npos);
-        if (has_delimiter)
+        if (delimiter_pos != std::string::npos && delimiter_pos > 0)
         {
-            std::string key = line.substr(0, delimiter_pos - 1);
-            std::string value = line.substr(delimiter_pos + 2);
+            // Extract and trim key
+            std::string key = line.substr(0, delimiter_pos);
+            key.erase(0, key.find_first_not_of(" \t"));
+            key.erase(key.find_last_not_of(" \t") + 1);
+
+            // Extract and trim value
+            std::string value = line.substr(delimiter_pos + 1);
+            value.erase(0, value.find_first_not_of(" \t"));
+            value.erase(value.find_last_not_of(" \t") + 1);
+
+            // Store the key-value pair
             config_[key] = value;
             logger.log(Logger::log_level::DEBUG, "Read key-value pair: [" + key + "][" + value + "]");
         }
